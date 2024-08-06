3bf0c122fece5ae2e1eddf1d0e665c3fa506076c
Sander de Ruiter <sdr@16april1973.nl>
2024-08-06 22:18:02 +0200

    Added token info endpoint

--- app/routers/token_v1.py
+++ app/routers/token_v1.py
@@ -20,6 +20,54 @@ router = APIRouter(tags=["Token"], prefix="/v1")
 
 
 @router.post(
+    "/{net}/token/{contract_index}/{contract_subindex}/{token_id}/info",
+    response_class=JSONResponse,
+)
+async def get_info_for_token_address(
+    request: Request,
+    net: str,
+    contract_index: int,
+    contract_subindex: int,
+    token_id: str | None,
+    mongodb: MongoDB = Depends(get_mongo_db),
+) -> JSONResponse:
+    """
+    Endpoint to get information for a given token address..
+    """
+    # token_id = "" if token_id == "_" else token_id
+    db_to_use = mongodb.testnet if net == "testnet" else mongodb.mainnet
+    token_address = f"<{contract_index},{contract_subindex}>-{token_id}"
+    token_from_collection = db_to_use[Collections.tokens_token_addresses_v2].find_one(
+        {"_id": token_address}
+    )
+
+    if token_from_collection:
+        # get mint event from the logged events collection
+        mint_event_logged_event = db_to_use[Collections.tokens_logged_events].find_one(
+            {"$and": [{"token_address": token_address}, {"event_type": "mint_event"}]}
+        )
+        token_from_collection.update(
+            {"mint_tx_hash": mint_event_logged_event["tx_hash"]}
+        )
+
+        # get current owner from the token_links collection
+        current_owner_link = db_to_use[Collections.tokens_links_v2].find_one(
+            {"token_holding.token_address": token_address}
+        )
+        token_from_collection.update(
+            {"current_owner": current_owner_link["account_address"]}
+        )
+        if "hidden" in token_from_collection:
+            del token_from_collection["hidden"]
+        return JSONResponse(token_from_collection)
+    else:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Requested token_id {token_id} from contract <{contract_index},{contract_subindex}> is not found on {net}.",
+        )
+
+
+@router.post(
     "/{net}/token/{contract_index}/{contract_subindex}/refresh",
     response_class=RedirectResponse,
 )
@@ -89,5 +137,5 @@ async def add_token_address_to_metadata_refresh_queue(
     else:
         raise HTTPException(
             status_code=404,
-            detail=f"Requested token_id({token_id}) from contract <{contract_index},{contract_subindex}> is not found on {net}.",
+            detail=f"Requested token_id {token_id} from contract <{contract_index},{contract_subindex}> is not found on {net}.",
         )
